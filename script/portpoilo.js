202b1687ae8ebb255de872e2d2fc7b484e9cad81
leejonghyunbeen <clfl19@naver.com>
2024-01-24 18:17:42 +0900

    폰트까지

---
+++ script/portpoilo.js
@@ -0,0 +1,8 @@
+const circle = document.querySelector('.circle')
+window.addEventListener('mousemove',(e)=>{
+    // console.log(`sceenX=${e.screenX}clienX=${e.clientX}`)
+    //  스크린은 모니터 클라이언트는 보이는 화면 기준
+    console.log(`clienX=${e.clientX}`)
+    circle.style.left = `${e.clientX}px`
+    circle.style.top = `${e.clientY}px`
+}) 
