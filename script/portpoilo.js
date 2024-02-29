32f55b71111d15f4b68f63b405748a7ec7e4e747
leejonghyunbeen <clfl19@naver.com>
2024-02-29 18:13:36 +0900

    안해

--- script/portpoilo.js
+++ script/portpoilo.js
@@ -7,3 +7,31 @@ window.addEventListener('mousemove',(e)=>{
     circle.style.top = `${e.clientY}px`
 }) 
 
+const popup = document.querySelector('.nav_popup')
+const popup_btn = document.querySelector('header .img')
+const close_btn =document.querySelector('.title .img a')
+const close_img =document.querySelector('.title .img a img')
+const title = document.querySelector('.first_menu h2')
+console.log(title)
+console.log(popup_btn)
+popup.style.display = 'none'
+popup_btn.addEventListener('click',()=>{
+    popup.style.display = 'block'
+})
+close_btn.addEventListener('click',()=>{
+    popup.style.display = 'none'
+})
+close_btn.addEventListener('mouseover',()=>{
+    close_img.src = './images/close.png'
+})
+close_btn.addEventListener('mouseout',()=>{
+    close_img.src = './images/close_a5a5a5.png'
+})
+
+
+
+title.addEventListener('mousemove', function (e) {
+    const tiltX = (e.clientX / window.innerWidth - 0.5) * 1800; 
+    title.style.transform = `perspective(1000px) rotateY(${tiltX}deg)`;
+    title.style.transition = '3s'
+});
