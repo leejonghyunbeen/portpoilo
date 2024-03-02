88a2cd0ef3d1d415221291f12f537e20780f2976
unknown <clfl19@naver.com>
2024-03-03 02:53:47 +0900

    main

--- script/portpoilo.js
+++ script/portpoilo.js
@@ -7,31 +7,37 @@ window.addEventListener('mousemove',(e)=>{
     circle.style.top = `${e.clientY}px`
 }) 
 
-const popup = document.querySelector('.nav_popup')
-const popup_btn = document.querySelector('header .img')
-const close_btn =document.querySelector('.title .img a')
-const close_img =document.querySelector('.title .img a img')
-const title = document.querySelector('.first_menu h2')
-console.log(title)
-console.log(popup_btn)
-popup.style.display = 'none'
-popup_btn.addEventListener('click',()=>{
-    popup.style.display = 'block'
-})
-close_btn.addEventListener('click',()=>{
-    popup.style.display = 'none'
-})
-close_btn.addEventListener('mouseover',()=>{
-    close_img.src = './images/close.png'
-})
-close_btn.addEventListener('mouseout',()=>{
-    close_img.src = './images/close_a5a5a5.png'
-})
+// const popup = document.querySelector('.nav_popup')
+// const popup_btn = document.querySelector('header .img')
+// const close_btn =document.querySelector('.title .img a')
+// const close_img =document.querySelector('.title .img a img')
+// const title = document.querySelector('.first_menu h2')
+// console.log(title)
+// console.log(popup_btn)
+// popup.style.display = 'none'
+// popup_btn.addEventListener('click',()=>{
+//     popup.style.display = 'block'
+// })
+// close_btn.addEventListener('click',()=>{
+//     popup.style.display = 'none'
+// })
+// close_btn.addEventListener('mouseover',()=>{
+//     close_img.src = './images/close.png'
+// })
+// close_btn.addEventListener('mouseout',()=>{
+//     close_img.src = './images/close_a5a5a5.png'
+// })
 
 
 
-title.addEventListener('mousemove', function (e) {
-    const tiltX = (e.clientX / window.innerWidth - 0.5) * 1800; 
-    title.style.transform = `perspective(1000px) rotateY(${tiltX}deg)`;
-    title.style.transition = '3s'
-});
+// title.addEventListener('mousemove', function (e) {
+//     const tiltX = (e.clientX / window.innerWidth - 0.5) * 1800; 
+//     title.style.transform = `perspective(1000px) rotateY(${tiltX}deg)`;
+//     title.style.transition = '3s'
+// });
+
+const start_animation = document.querySelector('.start_animation')
+const click = document.querySelector('.click')
+click.addEventListener('click',()=>{
+    start_animation.style.transform = 'translateY(100%)'
+})
