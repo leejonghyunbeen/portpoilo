e2fea81199cc738fbbecf47996f96ac1e407d215
leejonghyunbeen <clfl19@naver.com>
2024-03-04 17:14:52 +0900

    첫번째 중간완성

--- script/portpoilo.js
+++ script/portpoilo.js
@@ -7,6 +7,15 @@ window.addEventListener('mousemove',(e)=>{
 
 
 
+//네비
+const nav =document.querySelector('.left_nav')
+const nav_btn = document.querySelector('.nav_bar a')
+
+nav.style.display = 'none'
+nav_btn.addEventListener('click',(e)=>{
+    e.preventDefault()
+    nav.style.display = 'flex'
+})
 
 const start_animation = document.querySelector('.start_animation')
 const click = document.querySelector('.click')
@@ -30,11 +39,16 @@ bg.forEach((obj,index)=>{
 })
 nav_a.forEach((obj,index)=>{
     obj.addEventListener('click',(e)=>{
+        nav.style.display='none'
         e.preventDefault()
-        window.scrollTo(0,bg[index].offsetTop)
+        window.scrollTo(0,bg[`${index -1 }`].offsetTop)
     })
 })
 
+const close_btn =document.querySelector('.close a')
+close_btn.addEventListener('.click',()=>{
+    nav.style.display='none'
+})
 const skill_text = document.querySelectorAll('.img .contents p');
 const skill_a = document.querySelectorAll('.img .contents a');
 
@@ -54,18 +68,41 @@ skill_text.forEach((text, index) => {
     });
 });
 
+const show_more = document.querySelectorAll('.show_more')
+const show_text = document.querySelectorAll('.show_text')
+const show_close = document.querySelectorAll('.show_text img')
+show_text.forEach((text,index)=>{
+    text.style.opacity = '0'
+    text.style.transition = 'opacity 0.4s ease-in-out';
+    show_more[index].addEventListener('click',(e)=>{
+        e.preventDefault()
+        text.style.opacity = '1'; 
+    })
+    console.log(show_close[index])
+    show_close[index].addEventListener('click',(e)=>{
+        text.style.opacity = '0'; 
+        e.preventDefault()
+    })
+})  
 
-//스크롤
+const figma = document.querySelectorAll('.planning')
+figma.forEach((target,index)=>{
+    target.addEventListener('click',(e)=>{
+        e.preventDefault()
+        window.open('https://www.figma.com/file/d97P1f5xxpEmgM6TqXLsxs/2%EC%A1%B0-Wavve-%ED%8C%80-%ED%94%84%EB%A1%9C%EC%A0%9D%ED%8A%B8?type=design&node-id=374%3A813&mode=design&t=GXwnZEFb5zmLlabM-1','_blank')
+    })
+})
+const git =document.querySelectorAll('.git')
+git.forEach((target,index)=>{
+    target.addEventListener('click',(e)=>{
+        window.open ('https://github.com/EunsungGIT/TeanProjectWavve.git','_blank')
+    })
+})
 
 
-// /* 스와이프 */
-// let slide = new Swiper('.web_project',{
-//     slidesPerView: 1,
-// })
 
-
-
+//스와이프
 let slide = new Swiper('.benner_main',{
     slidesPerView: 1,
-    autoplay:{delay:3000}
+    // autoplay:{delay:3000}
 })
